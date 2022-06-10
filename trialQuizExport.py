f88f7e4f123d7376cea8aad4ef9b71b5b19221f9
wodoame <98283377+wodoame@users.noreply.github.com>
2022-06-10 02:10:16 +0100

    Update trialQuizExport.py

--- trialQuizExport.py
+++ trialQuizExport.py
@@ -14,16 +14,14 @@ def main():
         print("\ntime is up!!\nenter your final answer")
 
 
-
-
-
-    time_limiter = Thread(target=timer)
-    time_limiter.start()
+    def set_timer():
+        time_limiter = Thread(target=timer)
+        time_limiter.start()
 
     questions = {
                 
         1: "given two vectors a and b, what is the name for a x b or b x a ? >> ",
-        2: "i and k are standard basis vectors, i x k = >> ",
+        2: "i and k are standard basis vectors, i x k >> ",
         3: "the sum of all entries in the main diagonal of a matrix is called what? >> ",
         4: "how many ways can you arrange 5 distinct items in a single line? >> ",
         5: "what is the name of the invisible force that pulls objects to the surface of the earth >> ",
@@ -32,12 +30,8 @@ def main():
         8: "i and j are standard basis vectors i x j = >> ",
         9: "provided n is an integer, if n mod 2 == 0 then n has to be even\nTrue or False >> ",
         10: "what is the value of e^(ln(3.142)) >> "
-       
 
     }
-    
-    
-    
     answers = {
         
         1: "cross product",
@@ -51,37 +45,44 @@ def main():
         9: "true",
         10: "3.142"
     }
-    
-    
-    
-    
-    command = "y"
-    while command == "y":
-        j = randint(1, len(questions))
-        k = 1
-        score = 0
-        try:
-            while time_limit > 0:
-                answer = input(f"\nquestion {k}: {questions.get(j)}").lower().strip()
 
-                if answer == answers.get(j):
-                    print("that's correct")
-                    score += 5
-                else:
-                    print("that's incorrect")
-                j = randint(1, len(questions))
-                k += 1
-            print("\nscore = {}".format(score))
-        finally:
+    j = randint(1, len(questions))
+    k = 1
+    score = 0
+    set_timer()
+    do_loop = True
+    while do_loop:
+        while time_limit > 0:
+            answer = input(f"\nquestion {k}: {questions.get(j)}").lower().strip()
+
+            if answer == answers.get(j, "\nnone"):
+                print("that's correct")
+                score += 5
+            else:
+                print("that's incorrect")
+            j = randint(1, len(questions))
+            k += 1
+        print("\nyour score = {}".format(score))
+       
+        input_1 = True
+        while input_1:
             command = str(input("\ndo you want to retry(y/n): ")).strip()
             if command == "y":
-                time_limiter = Thread(target=timer)
-                time_limiter.start()
-                
+                j = randint(1, len(questions))
+                k = 1
+                score = 0
+                set_timer()
+                break
+            elif command == "n":
+                do_loop = False
+                break
+            else:
+                print("\n[wrong-input]; only 'y' or 'n' accepted")
 
 
-    
-    
+
+
+
 if __name__ == "__main__":
     main()
     
