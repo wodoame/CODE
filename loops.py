c5070d2b5a54145e830cd9644a0acdaa386cb2eb
wodoame <98283377+wodoame@users.noreply.github.com>
2022-06-11 10:38:42 +0100

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,43 @@
+class Int: 
+    def checkValueError(message, messageIfError=""):
+        check = True 
+        while check:
+            try: 
+                input_val = int(input(message))
+            except ValueError: 
+                print(messageIfError)
+            else: 
+                return input_val
+    
+
+    def checkZeroDivisionError(message, messageIfError=""):
+        check = True
+        while check: 
+            try: 
+                input_val = int(input(message))
+            except ZeroDivisionError:
+                print(messageIfError)
+            else: 
+                return input_val
+
+
+
+
+class String: 
+    def loopIfElse(message, *values, messageIfError= ""):
+        check = True 
+        while check: 
+            input_val = str(input(message)).lower().strip()
+            if input_val not in values: 
+                print(messageIfError)
+            else: 
+                return input_val 
+
+           
+        
+        
+
+
+
+
+
