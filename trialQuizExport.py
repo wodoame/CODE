4eec1d1f5708b2829dafc1def38bc8e6d5b1fb6f
wodoame <98283377+wodoame@users.noreply.github.com>
2022-06-11 12:13:33 +0100

    Update trialQuizExport.py

--- trialQuizExport.py
+++ trialQuizExport.py
@@ -1,57 +1,58 @@
 def main():
     """main function"""
+    from loops import String 
     from random import randint
     from time import sleep
     from threading import Thread
 
-    def timer():
-        """timing function"""
-        global time_limit
-        time_limit = 30
-        for i in range(time_limit):
-            time_limit -= 1
-            sleep(1)
-        print("\ntime is up!!\nenter your final answer")
-
-
-    def set_timer():
-        time_limiter = Thread(target=timer)
-        time_limiter.start()
-
-    questions = {
-                
-        1: "given two vectors a and b, what is the name for a x b or b x a ? >> ",
-        2: "i and k are standard basis vectors, i x k >> ",
-        3: "the sum of all entries in the main diagonal of a matrix is called what? >> ",
-        4: "how many ways can you arrange 5 distinct items in a single line? >> ",
-        5: "what is the name of the invisible force that pulls objects to the surface of the earth >> ",
-        6: "I am the field of mathematics that studies the likelihood of an event happening >> ",
-        7: "the difference between the maximum and minimum value of sin(x), where x is an angle, is: >> ",
-        8: "i and j are standard basis vectors i x j = >> ",
-        9: "provided n is an integer, if n mod 2 == 0 then n has to be even\nTrue or False >> ",
-        10: "what is the value of e^(ln(3.142)) >> "
-
-    }
-    answers = {
+    def main_logic(score):
         
-        1: "cross product",
-        2: "-j",
-        3: "trace",
-        4: "120",
-        5: "gravity",
-        6: "probability",
-        7: "2",
-        8: "k",
-        9: "true",
-        10: "3.142"
-    }
+        def timer():
+            """timing function"""
+            global time_limit
+            time_limit = 30
+            for i in range(time_limit):
+                time_limit -= 1
+                sleep(1)
+            print("\ntime is up!!\nenter your final answer")
 
-    j = randint(1, len(questions))
-    k = 1
-    score = 0
-    set_timer()
-    do_loop = True
-    while do_loop:
+
+        def set_timer():
+            time_limiter = Thread(target=timer)
+            time_limiter.start()
+
+        questions = {
+                    
+            1: "given two vectors a and b, what is the name for a x b or b x a ? >> ",
+            2: "i and k are standard basis vectors, i x k >> ",
+            3: "the sum of all entries in the main diagonal of a matrix is called what? >> ",
+            4: "how many ways can you arrange 5 distinct items in a single line? >> ",
+            5: "what is the name of the invisible force that pulls objects to the surface of the earth >> ",
+            6: "I am the field of mathematics that studies the likelihood of an event happening >> ",
+            7: "the difference between the maximum and minimum value of sin(x), where x is an angle, is: >> ",
+            8: "i and j are standard basis vectors i x j = >> ",
+            9: "provided n is an integer, if n mod 2 == 0 then n has to be even\nTrue or False >> ",
+            10: "what is the value of e^(ln(3.142)) >> "
+
+        }
+        answers = {
+            
+            1: "cross product",
+            2: "-j",
+            3: "trace",
+            4: "120",
+            5: "gravity",
+            6: "probability",
+            7: "2",
+            8: "k",
+            9: "true",
+            10: "3.142"
+        }
+
+        j = randint(1, len(questions))
+        k = 1
+        set_timer()
+        score = score 
         while time_limit > 0:
             answer = input(f"\nquestion {k}: {questions.get(j)}").lower().strip()
 
@@ -63,21 +64,14 @@ def main():
             j = randint(1, len(questions))
             k += 1
         print("\nyour score = {}".format(score))
-       
-        input_1 = False
-        while not input_1:
-            command = str(input("\ndo you want to retry(y/n): ")).strip()
-            if command == "y":
-                j = randint(1, len(questions))
-                k = 1
-                score = 0
-                set_timer()
-                break
-            elif command == "n":
-                do_loop = False
-                break
-            else:
-                print("\n[wrong-input]; only 'y' or 'n' accepted")
+    
+        command = String.loopIfElse("\ndo you want to retry(y/n) >> ", "y", "n")
+        
+        if command == "y":
+            main_logic(0)
+
+
+    main_logic(0)                
 
 
 
@@ -85,4 +79,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
