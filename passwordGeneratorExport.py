e4085b5cf84487d8cdb63ab38bb582b9aada2088
wodoame <98283377+wodoame@users.noreply.github.com>
2022-06-11 10:37:42 +0100

    Update passwordGeneratorExport.py

--- passwordGeneratorExport.py
+++ passwordGeneratorExport.py
@@ -1,42 +1,40 @@
-def main():
+ef main():
     import random
-    input_1 = False
-    while not input_1:
-        try:
-            n = int(input("\nhow many characters are in the password >> "))
-            sample = int(input("how many samples >> "))
-        except ValueError:
-            print("\n[wrong-input]; password and sample length can only be integers")
-            continue
-        else:
-            print()
-            stringLower = "abcdefghijklmnopqrstuvwxyz"
-            stringUpper = stringLower.upper()
-            numbers = "1234567890"
-            specialCharacters = "@.-&"
-            elements = [stringLower, stringUpper, numbers, specialCharacters]
-
-            for sample_range in range(sample):
-                result = []
-                for item in elements:
-                    result += random.choices(item, k=1)
-                r = n - 4
-                combination = "".join(elements)
-                result += random.choices(combination, k=r)
-                print(f"generated password >> {''.join(result)}")
-
+    from loops import Int, String
     
     
-        do_loop = True
-        while do_loop:
-            command = str(input("\ndo you want to retry?(y/n) >> ")).lower().strip()
-            if command == "y":
-                break
-            elif command == n:
-                input_1 = True
-                break
-            else:
-                print("\n[wrong-input]; only 'y' or 'n' accepted")
+    def algorithms(): 
+        n = Int.checkValueError("\nhow many characters are in the password >> ")
+        sample = Int.checkValueError("how many samples >> ")
+        
+        print()
+        stringLower = "abcdefghijklmnopqrstuvwxyz"
+        stringUpper = stringLower.upper()
+        numbers = "1234567890"
+        specialCharacters = "@.-&"
+        elements = [stringLower, stringUpper, numbers, specialCharacters]
+
+        for sample_range in range(sample):
+            result = []
+            for item in elements:
+                result += random.choices(item, k=1)
+            r = n - 4
+            combination = "".join(elements)
+            result += random.choices(combination, k=r)
+            print(f"generated password >> {''.join(result)}")
+
+
+    
+        
+        command = String.loopIfElse("\ndo you want to continue >> ", "y", "n")
+
+        if command == "y":
+            algorithms()
+    
+    
+    
+    
+    algorithms()
 
 
 
