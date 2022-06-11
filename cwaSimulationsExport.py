768c2930ac0d38e09b74fa0272fa2401a5d960ef
wodoame <98283377+wodoame@users.noreply.github.com>
2022-06-11 16:02:10 +0100

    Update cwaSimulationsExport.py

--- cwaSimulationsExport.py
+++ cwaSimulationsExport.py
@@ -1,19 +1,23 @@
 def main(): 
     from loops import Int, Float, String
-    # cwa calculations  #i = previous numerator ; j = previous denominator ; array stors cwas
-    def main_logic(i, j, array): # parameters are initial conditions  
+    # cwa calculations  # prev_num = previous numerator ; prev_den = previous denominator ; array stores cwas
+    
+    
+    def main_logic(prev_num, prev_den, array): # parameters are initial conditions  
         total_credits = 0 
-        points_obtained = 0
+        points_obtained = 0 # always starts at zero per semester  
         total_points = 0   
         course_count = Int.checkValueError("\nenter the number of courses >> ")
+
         for i in range(course_count):
             score = Float.checkValueError(f"\ncourse[{i + 1}]\nscore >> ")
             credit = Int.checkValueError("credit hour(s) >> ")
+            
             points_obtained += (score * credit)
             total_credits += credit 
         if total_credits != 0:
-            total_points = (total_credits * 100) + j
-            points_obtained += i
+            total_points = (total_credits * 100) + prev_den 
+            points_obtained += prev_num 
             cwa = (points_obtained / total_points) * 100
             array.append(cwa)
             print(f"\ncwa progress = {array};\ncurrent cwa = {cwa};\nfriction = {total_points/100}")
@@ -22,10 +26,9 @@ def main():
         command = String.loopIfElse("\ndo you want to continue calculating? >> ", "y", "n")
         if command == "y":
             print("\nNote: further calculations will be added to your previous results to make a new cwa")
-            i = points_obtained 
-            j = total_points 
-            main_logic(i, j, array)
-        
+            prev_num = points_obtained 
+            prev_den = total_points
+            main_logic(prev_num, prev_den, array)
     main_logic(0, 0, [])        
                         
         
@@ -46,3 +49,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+            
+
+
+
+
