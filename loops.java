5cdcdbb2a339160ae7c20259f4362d8e85dfd4df
wodoame <98283377+wodoame@users.noreply.github.com>
2022-06-12 21:21:19 +0100

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,89 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class loops{
+
+    public static int exceptionInt(String message, String messageIfError) {
+        Scanner input = new Scanner(System.in);
+        int input_val = 0;
+        boolean check = true;
+        while(check){
+
+            try {
+                System.out.print(message);
+                input_val = input.nextInt();
+            }
+            catch(InputMismatchException e){
+                System.out.println(messageIfError);
+                input.next();
+                continue;
+            }
+
+            break;
+
+        }
+
+        return input_val;
+    }
+
+
+
+
+    public static float exceptionFloat(String message, String messageIfError){
+        Scanner input = new Scanner(System.in);
+        float input_val = 0;
+        boolean check = true;
+        while(check){
+
+
+            try {
+                System.out.print(message);
+                input_val = input.nextFloat();
+            }
+            catch(InputMismatchException e){
+                System.out.println(messageIfError);
+                input.next();
+                continue;
+            }
+            break;
+
+        }
+
+        return input_val;
+    }
+
+
+
+
+
+
+    public static String loopIfElse(String message, String messageIfError, String... args) {
+        Scanner input = new Scanner(System.in);
+        boolean contains = false;
+        String input_val = "";
+
+        while (!contains) {
+            System.out.print(message);
+            input_val = input.nextLine().toLowerCase().trim();
+
+            for (String element : args) {
+                if (element.equals(input_val)) {
+                    contains = true;
+                    break;
+                }
+
+            }
+            if(!contains){
+                System.out.println(messageIfError);
+            }
+
+        }
+        return input_val;
+    }
+
+
+    //end
+}
+
+
