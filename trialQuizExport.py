5a922aeb58f64f658e22f51e45737dd5e0f48018
wodoame <98283377+wodoame@users.noreply.github.com>
2022-07-01 02:30:38 +0100

    Update trialQuizExport.py

--- trialQuizExport.py
+++ trialQuizExport.py
@@ -4,13 +4,18 @@ from time import sleep
 from threading import Thread
 
 
-def readFrom(filename: str, hashmap: dict):
+def readFrom(filename: str, hashmap: dict)->None:
     with open(filename, "r") as file:
         container = file.readlines()  # a list of all lines
+        char = "\\n"
         for line in container:
             dot = line.index(".")
-            key = int(line[0: dot])
+            key = int(line[0:dot])
             value = line[dot + 1:-1]
+            if char in value:
+                spacing = ' '* len(f"question_____")
+                strings = value.split(char)
+                value = f"\n{spacing}".join(strings)
             hashmap.update({key: value})
 
 
@@ -21,7 +26,10 @@ def timer():
     for i in range(time_limit):
         time_limit -= 1
         sleep(1)
-    print("\ntime is up!!\nenter final answer")
+
+    print("[time is up!!; enter final answer]", end=" >> ")
+
+
 
 
 
@@ -37,11 +45,12 @@ def main():
     filename_q = "C:\\Users\\WODOAME\\Desktop\\new_repository\\sample.txt"
     filename_a = "C:\\Users\\WODOAME\\Desktop\\new_repository\\answers.txt"
 
+    questions = {}
+    answers = {}
+    readFrom(filename_q, questions)
+    readFrom(filename_a, answers)
+
     def main_logic(score):
-        questions = {}
-        answers = {}
-        readFrom(filename_q, questions)
-        readFrom(filename_a, answers)
 
 
         j = randint(1, len(questions))
@@ -51,12 +60,13 @@ def main():
         while time_limit > 0:
             answer = input(f"\nquestion {k}: {questions.get(j)} >> ").lower().strip()
             if answer == answers.get(j):
-                print("that's correct")
+                print("[that's correct]")
                 score += 5
             else:
-                print("that's incorrect")
+                print("[that's incorrect]")
             j = randint(1, len(questions))
             k += 1
+
         print("\nyour score = {}".format(score))
     
         command = String.loopIfElse("\ndo you want to retry(y/n) >> ", "y", "n", messageIfError="[wrong-input]; only 'y' or 'n' accepted")
@@ -65,7 +75,7 @@ def main():
             main_logic(0)
 
 
-    main_logic(0)                
+    main_logic(0)            
 
 
 
@@ -73,4 +83,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
